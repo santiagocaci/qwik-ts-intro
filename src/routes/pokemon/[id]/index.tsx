361612059d87cb06907edbf2933fc145db52ea00
Santiago Caci <santiago.caci@gmail.com>
2023-05-18 09:16:21 -0300

    Pokemongame context implemented

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,6 +1,7 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useContext } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
+import { PokemonGameContext } from '~/context';
 import { PokemonImage } from '~/components/pokemons/pokemon-image';
 
 export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
@@ -15,10 +16,15 @@ export default component$(() => {
   const pokemonId = usePokemonId();
   // const loc = useLocation();
   // const id = loc.params.id;
+  const pokemonGame = useContext(PokemonGameContext);
   return (
     <>
       <div>Hello Pokemon Id: {pokemonId}</div>
-      <PokemonImage id={pokemonId.value} />
+      <PokemonImage
+        id={pokemonId.value}
+        isVisible={pokemonGame.isPokemonVisible}
+        isBack={pokemonGame.showBackImage}
+      />
     </>
   );
 });
